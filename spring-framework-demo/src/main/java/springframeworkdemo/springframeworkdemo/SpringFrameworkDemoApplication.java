9e631831193ec98a8e1a1d2aa1e67a1f8b71f31a
kodebae <heykarmendurbin@gmail.com>
2023-04-12 18:25:25 -0400

    initial commit

---
+++ spring-framework-demo/src/main/java/springframeworkdemo/springframeworkdemo/SpringFrameworkDemoApplication.java
@@ -0,0 +1,13 @@
+package springframeworkdemo.springframeworkdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFrameworkDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFrameworkDemoApplication.class, args);
+	}
+
+}
